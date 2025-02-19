4d448738d8d62abf7ecb1ca3de294bf4d998fa0d
KangHyunAh <fkslsn@gmail.com>
2025-02-19 14:47:27 +0900

    Debug

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private SpriteRenderer weaponPivot;
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
+    protected virtual void Awake() 
+    { 
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    protected virtual void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    protected virtual void Update()
     {
-        
+        Movement(movementDirection);
+        if (knockbackDuration > 0.0f) 
+        { 
+            knockbackDuration -= Time.deltaTime;
+        }
+    }
+
+    protected virtual void FixedUpdate() 
+    {
+        Movement(movementDirection);
+        if (knockbackDuration > 0.0f) 
+        { 
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movement(Vector2 direction) 
+    {
+        direction = direction * 5;
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+    }
+
+    public void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;
+    }
+
+    public void ApplyKnockback(Transform other,float power, float duration) 
+    { 
+        knockbackDuration = duration;
+        knockback =- (other.position - transform.position).normalized * power;
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Start() 
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+
+    protected override void HandleAction() 
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        //movementDirection 값 하나씩 추출해 사용할 수 있는 형태로 만들기
+        float moveX = movementDirection.x;
+        float moveY = movementDirection.y;
+
+        //바라보는 뱡향(a&<, d&>)에 따라 direction 바뀌도록
+        if(moveX == 0) //위, 아래로 움직이거나 움직이지 않을 때 
+        {
+            Debug.Log("애니메이션 정지");
+        }
+        else if(moveX != 0) 
+        { 
+        
+        }
+    }
+}
